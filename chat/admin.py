9c099a089f5388e3b4589f6f90745ffe4834d4b1
Xiaohan Xiang <x2003x05h28@gmail.com>
2026-04-15 13:06:14 -0400

    chap3_admin_modifiedl

--- chat/admin.py
+++ chat/admin.py
@@ -4,13 +4,13 @@ from .models import Conversation, Message
 
 @admin.register(Conversation)
 class ConversationAdmin(admin.ModelAdmin):
-    list_display = ("conversation_id", "title", "state", "created_at", "updated_at")
-    search_fields = ("conversation_id", "title")
+    list_display = ("id", "title", "state", "created_at")
+    search_fields = ("title",)
     list_filter = ("state",)
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ("message_id", "conversation", "role", "created_at")
-    search_fields = ("message_id", "content")
+    list_display = ("id", "role", "content", "created_at")
+    search_fields = ("content",)
     list_filter = ("role",)
